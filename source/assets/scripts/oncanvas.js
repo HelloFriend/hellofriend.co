c2f862cab7164e882c4ff4dd01a17e1456b80f08
Justin Threlkeld <justin.threlkeld@gmail.com>
2014-04-17 22:42:30 -0500

    really shit rough version

---
+++ source/assets/scripts/oncanvas.js
@@ -0,0 +1,12 @@
+function onCanvas(){
+  // $('#show-social, #social').on('click', function(){
+  //   console.log('showing social');
+  //   $('body').toggleClass('show-social');
+  // });
+
+  $('#js-show-nav, #nav').on('click', function(){
+    console.log('showing nav');
+    $('body').toggleClass('show-nav');
+  });
+}
+onCanvas();
